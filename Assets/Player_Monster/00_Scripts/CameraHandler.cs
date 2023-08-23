8d5f49967f9084a4505ba4ecb3c318b455c63f8b
Rua427 <ekrua427@naver.com>
2023-08-24 00:41:31 +0900

    player camera shaking 기능 추가

--- Assets/Player_Monster/00_Scripts/CameraHandler.cs
+++ Assets/Player_Monster/00_Scripts/CameraHandler.cs
@@ -35,8 +35,8 @@ public class CameraHandler : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        cameraTransform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        cameraTransform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
     }
 }
 
--- Assets/Player_Monster/00_Scripts/PlayerMovement.cs
+++ Assets/Player_Monster/00_Scripts/PlayerMovement.cs
@@ -10,31 +10,40 @@ public class PlayerMovement : MonoBehaviour
     public Transform orientation;
     PlayerInput playerInput;
 
+
     [HideInInspector]
     public Transform playerTransform;
+    private CharacterController characterController;
+    Vector3 moveDirection;
 
-    public new Rigidbody rigidbody;
-    public GameObject normalCamera;
+    [SerializeField] Transform playerCamera;
 
     [Header("Stats")]
     [SerializeField, Range(1, 10)] float walkSpeed = 5f;
     [SerializeField, Range(1, 15)] float sprintSpeed = 8f;
     [SerializeField, Range(1, 200)] float mass = 10f;
+    [SerializeField] bool canMove = true;
     [SerializeField] bool canSprint = true;
+    [SerializeField] bool canShaking = true;
     float thisSpeed;
 
-    Vector3 moveDirection;
-    [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
 
-    private CharacterController characterController;
+    [Header("Camera shake while moving")]
+    [SerializeField, Range(1, 10)] float shakingSpeed = 14f;
+    [SerializeField, Range(0f, 1f)] float yShakingAmount = .5f;
+    [SerializeField, Range(0f, 1f)] float xShakingAmount = .5f;
+    Vector3 defaultCamPos = Vector3.zero;
+    bool isShaking = false;
+    float timer = 0f;
+
+
 
     void Start()
     {
-        TryGetComponent(out rigidbody);
         TryGetComponent(out playerInput);
         TryGetComponent(out characterController);
+
+        defaultCamPos = playerCamera.localPosition;
 
         playerTransform = transform;
     }
@@ -47,11 +56,20 @@ public class PlayerMovement : MonoBehaviour
         // 플레이어 상태에 따른 처리
         PlayerState();
 
-        // 플레이어 이동량을 계산
-        MovementInput();
+        if (canMove)
+        {
+            // 플레이어 이동량을 계산
+            MovementInput();
 
-        // 플레이어 이동 처리
-        MovementPlayer();
+            if (canShaking)
+            {
+                CameraShaking();
+            }
+
+
+            // 플레이어 이동 처리
+            MovementPlayer();
+        }
     }
 
     #region Movement
@@ -66,13 +84,49 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * playerInput.vertical + orientation.right * playerInput.horizontal;
 
         if (moveDirection.magnitude > 1f) { moveDirection.Normalize(); }
+
+        moveDirection *= Time.deltaTime * thisSpeed;
     }
 
     void MovementPlayer()
     {
         if (!characterController.isGrounded) { moveDirection.y -= EnvironmentData.Instance.Gravity * mass * Time.deltaTime; }
 
-        characterController.Move(Time.deltaTime * thisSpeed * moveDirection);
+        characterController.Move(moveDirection);
+    }
+    #endregion
+
+    #region Camera
+    void CameraShaking()
+    {
+        if (!characterController.isGrounded) { return; }
+
+        // 이동량이 있을때만
+        if (playerInput.horizontal > .2f || playerInput.vertical > .2f)
+        {
+            isShaking = true;
+            timer += Time.deltaTime * shakingSpeed * thisSpeed;
+
+            playerCamera.localPosition
+            = new Vector3(defaultCamPos.x + Mathf.Sin(timer) * xShakingAmount,
+                          defaultCamPos.y + -Mathf.Abs(Mathf.Sin(timer)) * yShakingAmount,
+                          playerCamera.localPosition.z);
+        }
+        // .2f보다 떨어지면 입력을 안한것으로 간주
+        else
+        {
+            if (isShaking == false) { return; }
+
+            playerCamera.localPosition = Vector3.Lerp(playerCamera.localPosition, defaultCamPos, .33f);
+            timer = 0f;
+        }
+
+        if (playerInput.horizontal == 0f && playerInput.vertical == 0f)
+        {
+            playerCamera.localPosition = defaultCamPos;
+            isShaking = false;
+        }
+
     }
     #endregion
 }

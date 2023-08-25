66720b8fad0e31f887870b40ff365309ed8f2273
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-08-25 15:07:12 +0900

    feat: 총기 발사 변경 기능 추가
    총기 상태에 따른 총알 발사 변경 기능 추가

--- Assets/Script/GunMgr.cs
+++ Assets/Script/GunMgr.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GunMgr : MonoBehaviour
 {
@@ -9,25 +10,69 @@ public class GunMgr : MonoBehaviour
     public GameObject bullet;           // 총알 프리팹
     private float shootTime = 0.0f;     // 총알 발사 시간
     private float shootDelay = 0.1f;    // 총알 발사 딜레이
+    
+    private enum GunState { 
+        Empty,
+        Auto,
+        SemiAuto
+    };
+
+    private GunState gunState = GunState.Empty; // 총 상태
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         shootTime += Time.deltaTime; 
-
-        if(Input.GetMouseButton(0))
+        
+        // 총 상태 변경
+        if(Input.GetKeyDown(KeyCode.T))
         {
-            if(shootTime > shootDelay){
-                Instantiate(bullet, gun.transform.position, gun.transform.rotation);
-                shootTime = 0.0f;
+            // T 키를 누르면 총 상태 변경
+            gunState = (GunState)(((int)gunState + 1) % Enum.GetNames(typeof(GunState)).Length);
+            ChangedGunState(gunState);
+            Debug.Log(gunState);
+        }
 
+        if (Input.GetMouseButton(0)){
+            if (shootTime > shootDelay && gunState != GunState.Empty){          // 총이 비거나 총알 발사 시간이 지났을 때만 총알 발사
+                TryShoot(bullet);
+                shootTime = 0.0f;
             }
         }
+    }
+
+    // 총 상태 변경
+    private void ChangedGunState(GunState state)
+    {
+        switch (state)
+        {
+        case GunState.Empty:
+            // 비어있는 상태에서는 아무 작업도 하지 않음
+            break;
+
+        case GunState.Auto:
+            // Auto 상태에서는 계속 발사
+            shootDelay = 0.1f;
+            break;
+
+        case GunState.SemiAuto:
+            // SemiAuto 상태에서는 연사가 불가능하도록 발사 딜레이를 길게 설정
+            shootDelay = 0.5f;
+            break;
+        }
+    }
+
+    /// <summary>
+    /// 총알 발사
+    /// </summary>
+    /// <param name="bullet">총알 프리팹</param>
+    private void TryShoot(GameObject bullet){
+        Instantiate(bullet, gun.transform.position, gun.transform.rotation);
     }
 }

12606dc729e7239006d0d0734bcb2b6bbebead4d
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-08-25 15:47:05 +0900

    modify: GunMgr 속 변수, 함수 명을 이해하기 쉽도록 수정
    기존 변수, 함수명을 이해하기 쉽도록 수정

--- Assets/Script/GunMgr.cs
+++ Assets/Script/GunMgr.cs
@@ -11,13 +11,13 @@ public class GunMgr : MonoBehaviour
     private float shootTime = 0.0f;     // 총알 발사 시간
     private float shootDelay = 0.1f;    // 총알 발사 딜레이
     
-    private enum GunState { 
+    private enum GunModeState { 
         Empty,
         Auto,
         SemiAuto
     };
 
-    private GunState gunState = GunState.Empty; // 총 상태
+    private GunModeState gunState = GunModeState.Empty; // 총 상태
 
     // Start is called before the first frame update
     void Start()
@@ -34,34 +34,40 @@ public class GunMgr : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.T))
         {
             // T 키를 누르면 총 상태 변경
-            gunState = (GunState)(((int)gunState + 1) % Enum.GetNames(typeof(GunState)).Length);
-            ChangedGunState(gunState);
+            gunState = (GunModeState)(((int)gunState + 1) % Enum.GetNames(typeof(GunModeState)).Length);
+            SetFiringMode(gunState);
             Debug.Log(gunState);
         }
 
+        if(bulletCount <= 0)
+        {
+            gunState = GunModeState.Empty;
+            SetFiringMode(gunState);
+        }
+
         if (Input.GetMouseButton(0)){
-            if (shootTime > shootDelay && gunState != GunState.Empty){          // 총이 비거나 총알 발사 시간이 지났을 때만 총알 발사
+            if (shootTime > shootDelay && gunState != GunModeState.Empty){          // 총이 비거나 총알 발사 시간이 지났을 때만 총알 발사
                 TryShoot(bullet);
                 shootTime = 0.0f;
             }
         }
     }
 
-    // 총 상태 변경
-    private void ChangedGunState(GunState state)
+    // 총 발사 상태 변경
+    private void SetFiringMode(GunState state)
     {
         switch (state)
         {
-        case GunState.Empty:
+        case GunModeState.Empty:
             // 비어있는 상태에서는 아무 작업도 하지 않음
             break;
 
-        case GunState.Auto:
+        case GunModeState.Auto:
             // Auto 상태에서는 계속 발사
             shootDelay = 0.1f;
             break;
 
-        case GunState.SemiAuto:
+        case GunModeState.SemiAuto:
             // SemiAuto 상태에서는 연사가 불가능하도록 발사 딜레이를 길게 설정
             shootDelay = 0.5f;
             break;
